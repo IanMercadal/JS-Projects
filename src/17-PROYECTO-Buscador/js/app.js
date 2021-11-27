a9bad67490a809793c165e60486d262669a47bc1
Ian Mercadal <imercadal@cifpfbmoll.eu>
2021-11-27 20:07:02 +0100

    feat: :art: Funciones que lean atributos del objeto seleccionado
    Funciones que lean los valores seleccionados por el usuario

--- src/17-PROYECTO-Buscador/js/app.js
+++ src/17-PROYECTO-Buscador/js/app.js
@@ -1,9 +1,30 @@
 // Variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// Contenedor para los resultados
+const resultado = document.querySelector('#resultado');
+
 
 const max = new Date().getFullYear();
 const min = max - 10;
+
+// Generar un objeto con la búsqueda
+const datosBusqueda = {
+    marca : '',
+    year : '',
+    minimo : '',
+    maximo : '',
+    puertas : '',
+    transmision : '',
+    color : ''
+}
+
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
@@ -12,6 +33,32 @@ document.addEventListener('DOMContentLoaded', () => {
     // Llenar las opciones de año
     llenarSelect();
 })
+
+// Event listener para los select de búsqueda
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+});
+year.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;
+});
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+});
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+});
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;
+});
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+});
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+
+    console.log(datosBusqueda);
+})
+
 
 // Funciones
 function mostrarAutos(){
