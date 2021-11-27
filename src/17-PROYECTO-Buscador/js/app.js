56679fc5d4cb44437729f96fe43ab9c7a013ebe5
Ian Mercadal <imercadal@cifpfbmoll.eu>
2021-11-27 20:34:17 +0100

    feat: :art: Funciones que filtran por precio mínimo y máximo
    Creando nuevas funciones que filtran por precio máximo y mínimo

--- src/17-PROYECTO-Buscador/js/app.js
+++ src/17-PROYECTO-Buscador/js/app.js
@@ -44,9 +44,11 @@ year.addEventListener('change', e => {
 });
 minimo.addEventListener('change', e => {
     datosBusqueda.minimo = e.target.value;
+    filtrarAuto();
 });
 maximo.addEventListener('change', e => {
     datosBusqueda.maximo = e.target.value;
+    filtrarAuto();
 });
 puertas.addEventListener('change', e => {
     datosBusqueda.puertas = e.target.value;
@@ -98,7 +100,7 @@ function llenarSelect(){
 }
 
 function filtrarAuto(){
-    const resultado = autos.filter(filtrarMarca).filter(filterYear);
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo);
     // console.log(resultado)
 
     mostrarAutos(resultado);
@@ -111,10 +113,24 @@ function filtrarMarca(auto){
     return auto;
 }
 
-function filterYear(auto){
+function filtrarYear(auto){
     const {year} = datosBusqueda;
     if(year){
         return auto.year === year;
+    }
+    return auto;
+}
+function filtrarMinimo(auto){
+    const {minimo} = datosBusqueda;
+    if(minimo){
+        return auto.precio >= minimo;
+    }
+    return auto;
+}
+function filtrarMaximo(auto){
+    const {maximo} = datosBusqueda;
+    if(maximo){
+        return auto.precio <= maximo;
     }
     return auto;
 }
