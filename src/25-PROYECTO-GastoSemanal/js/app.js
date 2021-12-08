3841501eb836e24e2ed21c299c034adc1678272f
Ian Mercadal <imercadal@cifpfbmoll.eu>
2021-12-08 15:07:07 +0100

    feat: :art: Creando clases y funciones que muestran resultados a UI
    Clases que nos permiten mostrar presupuesto e insertarlo en el HTML

--- src/25-PROYECTO-GastoSemanal/js/app.js
+++ src/25-PROYECTO-GastoSemanal/js/app.js
@@ -9,6 +9,29 @@ function eventListeners() {
 }
 // Clases
 
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
+
+class UI {
+    insertarPresupuesto(cantidad) {
+        // Extraer valores
+        const {presupuesto, restante} = cantidad;
+
+        // Agregar HTML
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
+}
+
+// Instanciar
+const ui = new UI();
+let presupuesto;
+
 // Funciones
 function preguntarPresupuesto() {
     const preguntarUsuario = prompt('¿Cuál es el presupuesto?');
@@ -16,4 +39,7 @@ function preguntarPresupuesto() {
     if(preguntarUsuario === '' || preguntarUsuario === null || isNaN(preguntarUsuario) || preguntarUsuario <= 0) {
         window.location.reload();
     }
+    presupuesto = new Presupuesto(preguntarUsuario);
+
+    ui.insertarPresupuesto(presupuesto);
 }
