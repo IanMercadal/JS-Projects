d48ffb393fd3ba0bab5dd4334053cfd139f894d1
Ian Mercadal <imercadal@cifpfbmoll.eu>
2021-12-17 11:50:09 +0100

    feat: :art: Mostrando citas en HTMLy corrigiendo el error de alerta
    Nueva función que muestra la cita y correción para mostrar mensaje de error

--- src/26-PROYECTO-AdministrarCitas/js/app.js
+++ src/26-PROYECTO-AdministrarCitas/js/app.js
@@ -44,6 +44,64 @@ class UI {
             divMensaje.remove();
         }, 5000)
     }
+    imprimirCitas({citas}) {
+
+        this.limpiarHTML();
+
+        citas.forEach( cita => {
+            const {mascota, propietario, telefono, fecha, hora, sintomas, id} = cita;
+
+            const divCita = document.createElement('div');
+            divCita.classList.add('cita', 'p-3');
+            divCita.dataset.id = id;
+
+            // Scripting de los elementos de la cita
+            const mascotaParrafo = document.createElement('h2');
+            mascotaParrafo.classList.add('card-title', 'font-weight-bolder');
+            mascotaParrafo.textContent = mascota;
+
+            const propietarioParrafo = document.createElement('p');
+            propietarioParrafo.innerHTML = `
+                <span class="font-weight-bolder">Propietario:</span> ${propietario}
+            `;
+
+            const telefonoParrafo = document.createElement('p');
+            telefonoParrafo.innerHTML = `
+                <span class="font-weight-bolder">Telefono:</span> ${telefono}
+            `;
+
+            const fechaParrafo = document.createElement('p');
+            fechaParrafo.innerHTML = `
+                <span class="font-weight-bolder">Fecha:</span> ${fecha}
+            `;
+
+            const horaParrafo = document.createElement('p');
+            horaParrafo.innerHTML = `
+                <span class="font-weight-bolder">Hora:</span> ${hora}
+            `;
+
+            const sintomasParrafo = document.createElement('p');
+            sintomasParrafo.innerHTML = `
+                <span class="font-weight-bolder">Síntomas:</span> ${sintomas}
+            `;
+
+            // Agregar los parrafos al divCita
+            divCita.appendChild(mascotaParrafo);
+            divCita.appendChild(propietarioParrafo);
+            divCita.appendChild(telefonoParrafo);
+            divCita.appendChild(fechaParrafo);
+            divCita.appendChild(horaParrafo);
+            divCita.appendChild(sintomasParrafo);
+
+            // Agregar las citas al HTML
+            contenedorCitas.appendChild(divCita);
+        } )
+    }
+    limpiarHTML() {
+        while(contenedorCitas.firstChild) {
+            contenedorCitas.removeChild(contenedorCitas.firstChild);
+        }
+    }
 }
 
 const ui = new UI();
@@ -84,7 +142,7 @@ function nuevaCita(e) {
 
     // validar
     if( mascota === ''|| propietario === '' || telefono === '' || fecha === ''|| hora === '' || sintomas === '' ) {
-        ui.imprimirAlerta('Todos los campos son obligatorios');
+        ui.imprimirAlerta('Todos los campos son obligatorios', "error");
 
         return;
     }
@@ -102,6 +160,7 @@ function nuevaCita(e) {
     formulario.reset();
 
     // Mostrar el HTML de las citas
+    ui.imprimirCitas(administrarCitas);
 }
 
 function reiniciarObjeto() {
