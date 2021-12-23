dff0267574d53ed33c11936f071ca901ede026eb
Ian Mercadal <imercadal@cifpfbmoll.eu>
2021-12-23 20:03:22 +0100

    feat: :art: Mostrando en el DOM los datos
    Funciones que muestran en el DOM los datos de la Cripto solicitada

--- src/39-PROYECTO-Criptomonedas/js/app.js
+++ src/39-PROYECTO-Criptomonedas/js/app.js
@@ -1,4 +1,12 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
+
+const objBusqueda = {
+    moneda : '',
+    criptomoneda : ''
+}
 
 // Crear un Promise
 const obtenerCriptomonedas = criptomonedas => new Promise (resolve => {
@@ -7,6 +15,11 @@ const obtenerCriptomonedas = criptomonedas => new Promise (resolve => {
 
 document.addEventListener('DOMContentLoaded',() => {
     consultarCriptomonedas();
+
+    formulario.addEventListener('submit',submitFormulario);
+
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
 })
 
 function consultarCriptomonedas() {
@@ -27,4 +40,87 @@ function selectCriptomonedas(criptomonedas) {
         option.textContent = FullName;
         criptomonedasSelect.appendChild(option);
     })
+}
+function leerValor(e) {
+    objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e) {
+    e.preventDefault();
+
+    // Validar
+    const {moneda, criptomoneda} = objBusqueda;
+
+    if(moneda === '' || criptomoneda === '') {
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+
+    // Consultar la API con los resultados
+    consultarAPI();
+}
+
+function mostrarAlerta(mensaje) {
+    const existeError = document.querySelector('.error');
+
+    if(!existeError) {
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('error');
+
+        // Mensaje de error
+        divMensaje.textContent = mensaje;
+
+        formulario.appendChild(divMensaje);
+
+        setTimeout( () => {
+            divMensaje.remove();
+        }, 3000);
+    }
+}
+
+function consultarAPI() {
+    const {moneda, criptomoneda} = objBusqueda;
+
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(cotizacion => {
+            mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]);
+        })
+}
+
+function mostrarCotizacionHTML(cotizacion) {
+
+    limpiarHTML();
+
+    const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE} = cotizacion;
+
+    const precio = document.createElement('p');
+    precio.classList.add('precio');
+    precio.innerHTML = `El precio es: <span>${PRICE}</span>`;
+
+    const precioAlto = document.createElement('p');
+    precioAlto.innerHTML = `<p>Precio más alto del día <span>${HIGHDAY}</span></p>`;
+
+    const precioBajo = document.createElement('p');
+    precioAlto.innerHTML = `<p>Precio más bajo del día <span>${LOWDAY}</span></p>`;
+
+    const ultimasHoras = document.createElement('p');
+    ultimasHoras.innerHTML = `<p>Variación de las últimas 24 horas <span>${CHANGEPCT24HOUR}%</span></p>`;
+
+    const ultimaActualizacion = document.createElement('p');
+    ultimaActualizacion.innerHTML = `<p>Última Actualización <span>${LASTUPDATE}</span></p>`;
+
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(ultimasHoras);
+    resultado.appendChild(ultimaActualizacion);
+}
+
+function limpiarHTML() {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
 }
