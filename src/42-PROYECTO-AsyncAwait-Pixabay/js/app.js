ab8ace0a933eaf0e094abddfc4ad9c0acd15619c
Ian Mercadal <imercadal@cifpfbmoll.eu>
2021-12-24 12:20:35 +0100

    feat: :art: Pasando anteriores API's a asyncronas
    Modificando las funciones de consulta fetch a async

--- src/42-PROYECTO-AsyncAwait-Pixabay/js/app.js
+++ src/42-PROYECTO-AsyncAwait-Pixabay/js/app.js
@@ -48,19 +48,28 @@ function mostrarAlerta(mensaje) {
 
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
 
     const termino = document.querySelector('#termino').value;
 
-    const key = '';
+    const key = '24952128-e74da8e92c4d0624fa43ecae6';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
+    // fetch(url)
+    //     .then(respuesta => respuesta.json())
+    //     .then(resultado => {
+    //         totalPaginas = calcularPaginas(resultado.totalHits);
+    //         mostrarImagenes(resultado.hits);
+    //     })
+
+        try{
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
             totalPaginas = calcularPaginas(resultado.totalHits);
             mostrarImagenes(resultado.hits);
-        })
+        } catch (error) {
+            console.log('Error')
+        }
 }
 
 // Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
