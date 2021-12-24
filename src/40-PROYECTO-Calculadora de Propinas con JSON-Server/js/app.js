81af45e58ae2640478363a7be59730c5ecd20d85
Ian Mercadal <imercadal@cifpfbmoll.eu>
2021-12-24 19:59:54 +0100

    feat: :art: Creando formulario de propinas
    Insertando en el DOM la base del formulario

--- src/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ src/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -157,7 +157,7 @@ function actualizarResumen() {
     const contenido = document.querySelector('#resumen .contenido');
 
     const resumen = document.createElement('DIV');
-    resumen.classList.add('col-md-6', 'card', 'py-5', 'px-3','shadow');
+    resumen.classList.add('col-md-6', 'card', 'py-2', 'px-3','shadow');
 
     // Información de la mesa
     const mesa = document.createElement('P');
@@ -257,12 +257,15 @@ function actualizarResumen() {
     })
 
     // Agregando al contenido
+    resumen.appendChild(heading);
     resumen.appendChild(mesa);
     resumen.appendChild(hora);
-    resumen.appendChild(heading);
     resumen.appendChild(grupo);
 
     contenido.appendChild(resumen);
+
+    // Mostrar Formulario de propinas
+    formularioPropinas();
 }
 
 function limpiarHTML() {
@@ -306,4 +309,23 @@ function mensajePedidoVacio() {
     texto.textContent = 'Añade los elementos del pedido';
 
     contenido.appendChild(texto);
+}
+
+function formularioPropinas() {
+    const contenido = document.querySelector('#resumen .contenido');
+
+    const formulario = document.createElement('DIV');
+    formulario.classList.add('col-md-6','formulario');
+
+    const divFormulario = document.createElement('DIV');
+    divFormulario.classList.add('card','py-2','px-3','shadow');
+
+    const heading = document.createElement('H3');
+    heading.classList.add('my-4','text-center');
+    heading.textContent = 'Propina';
+
+    divFormulario.appendChild(heading);
+    formulario.appendChild(divFormulario);
+
+    contenido.appendChild(formulario);
 }
