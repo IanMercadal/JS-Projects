eceb6ddb7a6d2269dfece86b7e2833fb084d2fd9
Ian Mercadal <imercadal@cifpfbmoll.eu>
2021-12-24 19:13:40 +0100

    feat: :art: Función que limpia el HTML previo
    Función que muestra la info de forma correcta, sin horas repetidas

--- src/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ src/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -140,6 +140,9 @@ function agregarPlatillo(producto) {
         cliente.pedido = [...resultado];
     }
 
+    // Limpiar HTML
+    limpiarHTML();
+
     // Mostrar el resumen
     actualizarResumen();
 }
@@ -148,7 +151,7 @@ function actualizarResumen() {
     const contenido = document.querySelector('#resumen .contenido');
 
     const resumen = document.createElement('DIV');
-    resumen.classList.add('col-md-6');
+    resumen.classList.add('col-md-6', 'card', 'py-5', 'px-3','shadow');
 
     // Información de la mesa
     const mesa = document.createElement('P');
@@ -172,7 +175,23 @@ function actualizarResumen() {
     mesa.appendChild(mesaSpan);
     hora.appendChild(horaSpan);
 
+    // Titulo de la sección
+    const heading = document.createElement('H3');
+    heading.textContent = 'Platillos Consumidos';
+    heading.classList.add('my-4','text-center');
+
     // Agregando al contenido
-    contenido.appendChild(mesa)
-    contenido.appendChild(hora)
+    resumen.appendChild(mesa);
+    resumen.appendChild(hora);
+    resumen.appendChild(heading);
+
+    contenido.appendChild(resumen);
+}
+
+function limpiarHTML() {
+    const contenido = document.querySelector('#resumen .contenido');
+
+    while (contenido.firstChild) {
+        contenido.removeChild(contenido.firstChild);
+    }
 }
